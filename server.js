dc0548cff179b934e524076740ebe72125a60af9
Diego Maicon <diego_maicon20@hotmail.com>
2019-07-23 22:04:04 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir =require('require-dir');
+//Iniciando o APP
+const app = express();
+app.use(express.json());
+app.use(cors());
+ 
+
+//Iniciando o BANCO
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser:true});
+
+
+requireDir('./src/models');
+
+//Importa rotas
+app.use('/api',require('./src/routes'))
+
+app.listen(3001);
